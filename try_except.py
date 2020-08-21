24d690b4b8d26e141c29c64d68fcbcff2aee4410
aditya mangal <adityamangal0202@gmail.com>
2020-08-22 00:45:55 +0530

    try_except

---
+++ try_except.py
@@ -0,0 +1,11 @@
+try:
+    num1 = int(input("TELL ME YOUR AGE\n"))
+    num2 = int(input("TELL ME YOUR name\n"))
+
+    print(num1 + num2)
+    # print(num2)
+
+except Exception as dfdfe:
+    print(dfdfe)
+print("HELLO\n")
+

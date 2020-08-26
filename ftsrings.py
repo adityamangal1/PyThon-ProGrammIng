7511e763dccf1867168e359bd9f25dc128385514
aditya mangal <adityamangal0202@gmail.com>
2020-08-26 11:44:40 +0530

    fstring-examp

---
+++ ftsrings.py
@@ -0,0 +1,19 @@
+import math
+adi = "aditya"
+adi2 = "mangal"
+
+print(f"hello my name is {adi} {adi2}\n")
+print(math.tan(2))
+
+# F strings
+
+me = "Harry"
+a1 =3
+# a = "this is %s %s"%(me, a1)
+a = "This is {1} {0}"
+b = a.format(me, a1)
+print(b)
+# a = f"this is {me} {a1} {math.cos(65)}"
+# time
+# print(a)
+  

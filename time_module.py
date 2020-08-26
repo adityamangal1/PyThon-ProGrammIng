b71db91f1c503802990e2d4623271b8cdf62820c
aditya mangal <adityamangal0202@gmail.com>
2020-08-26 11:42:46 +0530

    time

---
+++ time_module.py
@@ -0,0 +1,36 @@
+import time
+# initial = time.time()
+
+# k = 0
+# while(k<45):
+#     print("This is harry bhai")
+#     time.sleep(2)
+#     k+=1
+# print("While loop ran in", time.time() - initial, "Seconds")
+
+# initial2 =time.time()
+# for i in range(5):
+#     print("This is harry bhai")
+# print("For loop ran in",time.time() - initial2, "Seconds")
+
+
+# localtime = time.asctime(time.localtime(time.time()))
+# # localtime = time.time()
+# print(localtime)
+  
+
+# inp = input("WHAT IS YOUR NAME?\n")  
+# print("WAIT FOR WHILE WE ARE GENERATING YOUR PASSWORD!!")
+# time.sleep(5)   #awesome function
+# print("YOUR TOKEN IS",inp)
+
+# adi = time.time()
+# for i in range(5):
+#     print(i+1)
+# print("THE TIME WE TAKE FOR THIS LOOP IS",time.time() - adi)
+
+mangal = time.asctime(time.localtime(time.time()))
+print(mangal)
+
+# mangal2 = time.asctime() 
+# print(mangal2)

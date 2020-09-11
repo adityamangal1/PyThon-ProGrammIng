da8e42f3b156976e9bd755c6c8b4df26cb2a0b09
aditya mangal <adityamangal0202@gmail.com>
2020-09-11 18:33:32 +0530

    initial commit

---
+++ slicing.py
@@ -0,0 +1,16 @@
+my_str = "python is the best language"
+# print(my_str[::-1])
+# print(my_str[:6:-2])
+# print(my_str[::3])
+# print(my_str[:6:2])
+
+my_str2 = 'hacker'
+# print(my_str2[0:6:2])
+# print(my_str2[1:6:2])
+
+# print(my_str[-5:-2])
+# print(my_str[:-2])
+
+print(my_str[0:6],end=" ")
+print(my_str[10:13],end=" ")
+print(my_str[14:18])

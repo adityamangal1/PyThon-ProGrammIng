ab9cb13e1556f5c220d89549216268cf7deb1991
aditya mangal <adityamangal0202@gmail.com>
2020-09-29 20:39:31 +0530

    initial commit

---
+++ other_modules.py
@@ -0,0 +1,15 @@
+
+# import PySimpleGUI as sg
+
+# sg.popup('About:', 'Created by Aditya mangal.', 'Virtual Assistant:' + '\n\n' + 'Alpha', 'Version 1.0')
+# sg.theme(settings['theme'])
+# sg.popup_animated('About:', 'Aditya mangal')
+# sg.popup('money:','100')
+# sg.popup_error('no money')
+# sg.popup_annoying('hatt')
+
+
+
+
+
+
